a6a6128bea6641f5b8d769d859d82c9011836151
Hemant <hemant.shelar@gmail.com>
2023-02-18 22:39:07 +1100

    Add customer via Mediator working!

---
+++ best.practices.core.6/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using best.practices.core._6.ApiDto;
+using best.practices.core._6.Handlers;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace best.practices.core._6.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
+		public CustomerController(IMediator mediator)
+		{
+			Mediator = mediator;
+		}
+		public IMediator Mediator { get; }
+
+		[HttpPost()]
+		public async Task<ActionResult<CustomerDTO>> Create(CustomerDTO newCustomer,CancellationToken cancellationToken)
+		{
+
+			var cutomerCommand = new CreateCustomerCommand(newCustomer.Name, newCustomer.TwitterHandle);
+			var result = await Mediator.Send(cutomerCommand, cancellationToken);
+			return Created("xyz", result);
+
+		}
+	}
+}
